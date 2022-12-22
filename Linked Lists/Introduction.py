7e8a8ef655f5d50f30ec484bf9d7532a1a5f6997
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2022-12-21 20:40:11 -0600

    .

--- Linked Lists/Introduction.py
+++ Linked Lists/Introduction.py
@@ -10,15 +10,20 @@ class Node:
     def _str_(self):
         return str(self.Value)
 
+#Create the Linked List
+
 class LinkedList:
     def _init_(self)
         self.First = None
         self.Size = 0 
-
-    def Append(self, Value):
+ 
+    def Append(self, Value):      #Method to append a node in the final of the list
         MyNode = Node(Value)
         if self.Size == 0:
             self.First = MyNode
         else:
             Current = self.First
-            
+            while(Current.next!=None):
+                Current = Current.next
+                
+
