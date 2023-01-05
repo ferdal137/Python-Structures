36bb8418cfc3f8e85e11b741963e015b424d0e74
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-01-04 20:50:10 -0600

    .

--- Stack/Intro.py
+++ Stack/Intro.py
@@ -1,1 +1,12 @@
 
+class Stack:
+    def __init__(self):
+        self.Top = None
+        self.Stack = []
+
+    def GetTop(self):
+        return self.Top
+    
+    def Add(self, Element):
+        self.Top = Element
+        self.Stack.append(Element)
