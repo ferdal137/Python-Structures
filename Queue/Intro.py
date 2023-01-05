b42a75ec353173651b9ef83ddd90e0753626bdc5
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-01-04 20:59:19 -0600

    .

--- Queue/Intro.py
+++ Queue/Intro.py
@@ -1,1 +1,17 @@
 
+
+class Queue:
+    def __init__(self):
+        self.Initial = None
+        self.Final = None
+        self.Queue = []
+
+    def GetInitial(self):
+        return self.GetInitial
+
+    def GetFinal(self):
+        return self.Final
+    
+    def Add(self, Element):
+        self.Queue.append(Element)
+        self.Final
